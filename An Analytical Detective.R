fe07bbf21e6050740f558e74aa74900a8f87754a
kichu1990 <34007976+kichu1990@users.noreply.github.com>
2018-03-28 18:34:09 +0100

    Update An Analytical Detective.R

--- An Analytical Detective.R
+++ An Analytical Detective.R
@@ -40,7 +40,28 @@ table(Detective_Analysis$Weekday)
 table(Detective_Analysis$Month,Detective_Analysis$Arrest)
 
 #For what proportion of motor vehicle thefts in 2001 was an arrest made?
-prop.table(table(Detective_Analysis$Arrest,Detective_Analysis$Year=="2001"))
+table(Detective_Analysis$Arrest,Detective_Analysis$Year)
+2152/(18517+2152)
 
 #For what proportion of motor vehicle thefts in 2007 was an arrest made?
-prop.table(table(Detective_Analysis$Year=="2007"))
+table(Detective_Analysis$Arrest,Detective_Analysis$Year)
+1212/(13068+1212)
+
+#For what proportion of motor vehicle thefts in 2012 was an arrest made?
+table(Detective_Analysis$Arrest,Detective_Analysis$Year)
+550/(13542+550)
+
+#Which locations are the top five locations for motor vehicle thefts, excluding the 
+#"Other" category? You should select 5 of the following options.
+head(sort(table(Detective_Analysis$LocationDescription),decreasing=TRUE),6)
+
+#Create a subset of your data, only taking observations for which the theft 
+#happened in one of these five locations, and call this new data set "Top5".
+TopLocations = c("STREET", "PARKING LOT/GARAGE(NON.RESID.)", "ALLEY", "GAS STATION", "DRIVEWAY - RESIDENTIAL")
+Top5=subset(Detective_Analysis,Detective_Analysis$LocationDescription%in%TopLocations)
+
+#How many observations are in Top5?
+nrow(Top5)
+
+#On which day of the week do the most motor vehicle thefts at gas stations happen?
+table(Top5$LocationDescription,Top5$Weekday)
